ebb47286b39703f22774079cc8599109294b28fd
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2018-08-17 06:21:01 +0000

    Lab 3

---
+++ Lab3_Q1.cpp
@@ -0,0 +1,22 @@
+// including the library
+#include<iostream>
+using namespace std;
+//writing the function
+int main() {
+//declaring data types 
+int a; 
+float b;
+char c;
+bool d;
+double e;
+//print the results
+cout << "size of int is "<< sizeof (a) <<endl;
+cout <<"sise of float is "<< sizeof (b) <<endl;
+cout <<"size of char is "<< sizeof (c) <<endl;
+cout <<"size of bool is "<< sizeof (d) <<endl;
+cout <<"size of double is "<< sizeof (e) <<endl;
+
+return 0;
+
+
+}
---
+++ Lab3_Q2.cpp
@@ -0,0 +1,23 @@
+// including the library
+#include<iostream>
+using namespace std; 
+//write the function
+int main(){ 
+//declaring variables:
+int a, b, sum;
+float c, d;
+//assigning values
+a = 35;
+b = 40;
+c = 3.5;
+d = 45;
+//process
+sum = a + b;
+cout <<sum <<endl;
+cout <<a * b <<endl;
+cout <<b % a <<endl;
+cout <<d / c <<endl;
+
+return 0;
+}
+
