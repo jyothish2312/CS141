24ed9d902f7f27f095196d48b69c10bf6c985f8e
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2018-08-29 09:51:22 +0000

    LAB 4 :(...FORGOT TO UPLOAD ON GITHUB:(:(

---
+++ Lab4_Q5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+//declaring variables
+int A , B , C;
+//introduction note
+cout<<"hello there...this is being the holy soul of JyozzzY....here's the game... you feed me in two angles of any triangle and ill tell you the third angle.... come on now..... feed me in the two angles(A,B)_______:)"<<endl;
+//seeking inputs
+cin>>A;
+cin>>B;
+//relations
+C=180-(A+B);
+//output declaring
+cout<<"there you go: the third angle of your triangle measures like____:"<<C<<endl;
+
+return 0;
+}
---
+++ Lab4_Q7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout<<"THIS PROGRAM WILL TELL YOU THE AREA OF AN EQUILATERAL TRIANGLE BY THE INPUT OF ONE SIDE"<<endl;
+cout<<"*****this program is written by JYOTHISH KUMAR J//Jyozzz"<<endl;
+
+int l , a;
+cout<<"PLEASE INPUT THE LENGTH OF ONE SIDE_______:"<<endl;
+cin>>l;
+
+a=(1.732/4)*l*l;
+
+cout<<"THE AREA OF THE GIVEN TRIANGLE IS_____:"<<a<<endl;
+
+return 0;
+
+}
